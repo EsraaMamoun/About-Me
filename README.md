30314628b0988b25ab3a4c7a7b8db5cfd972b3ba
EsraaMamoun <esraamamoun5@gmail.com>
2020-02-18 20:45:12 +0200

    commited

--- README.md
+++ README.md
@@ -4,3 +4,6 @@
 
 
 > I would like to thank people interested in visiting my site.
+
+
+`I am added some information work experience about me, and +2 quastions in my site`
--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 `use strict`
-
+var correct=0;
 var name = prompt("Whats your name?");
 alert(`Welcome ${name}, I wanna play with you a guess game.`);
 
@@ -15,6 +15,7 @@ switch (Q1.toLowerCase()) {
     case 'no':
     case 'n':
         alert('Yes, thats right, but I am forced to drink it');
+        correct=correct+1;
         console.log('Do you think am I love coffe?', Q1);
         break;
     default:
@@ -35,6 +36,7 @@ switch (Q2.toUpperCase()) {
     case 'N':
         alert("Yes unfortunately, pray for me to become good in this");
         console.log('do you think I am a good in English language', Q2);
+        correct=correct+1;
         break;
 
     default:
@@ -48,6 +50,7 @@ switch (Q3.toLowerCase()) {
     case 'yes':
     case 'y':
         alert("Yes, I am skilled in playing football");
+        correct=correct+1;
         console.log('Do you think can I play football?', Q3);
         break;
     case 'no':
@@ -68,6 +71,7 @@ switch (Q4.toLowerCase()) {
     case 'yes':
     case 'y':
         alert("Yes, I watched a hundreds");
+        correct=correct+1;
         console.log('Do you think am I watch Anime?', Q4);
         break;
     case 'no':
@@ -88,6 +92,7 @@ switch (Q5.toUpperCase()) {
     case 'Yes':
     case 'Y':
         alert('Yesss, his name is White(but in Arabic)');
+        correct=correct+1;
         console.log('Do you think am I own a cat?', Q5);
         break;
     case 'No':
@@ -101,4 +106,69 @@ switch (Q5.toUpperCase()) {
         break;
 }
 
+
+
+for (var i=1 ; i<=4 ; i++){
+    var guessNum = prompt('guess a number from 1 to 100');
+    if (guessNum > 100){
+        console.log("Error, Can not be bigger than 100");
+        alert('Error, Can not be bigger than 100');
+    }
+    else if (guessNum < 0){
+        alert('Error, Can not be smaller than 0');
+        console.log("Error, Can not be smaller than 0");
+    }
+    
+    else if (guessNum < 70){
+        alert('too low');
+        console.log("too low");
+    }
+    else if (guessNum > 70){ 
+        alert('too high');
+        console.log("too high");
+    }
+    else if (guessNum == 70){
+        alert("You are correct, the answer is " + 70);
+        correct=correct+1;
+        console.log("You are correct, the answer is " , 70);
+        alert("It took you " + i +" tries");
+        console.log("It took you " , i ," tries");
+        break;
+    }
+    else {
+         alert('It is a guess number game!');
+    }
+         
+    }  
+
+
+     
+    var favColor =['blue','black','yellow','orange'];
+    var answer = prompt('Can you guess whats my favourte color');
+    for (var j=1 ; j<6 ; j++){
+       var atempts=6-j;
+        if(favColor[0] == answer)
+        {
+            alert('Yes, it is a one of my favourte colore');
+            correct=correct+1;
+            break;
+        }else if(favColor[1] == answer){
+            alert('Yes, it is a one of my favourte colore');
+            correct=correct+1;
+            break;
+         }else if(favColor[2] == answer){
+            alert('Yes, it is a one of my favourte colore');
+            correct=correct+1;
+            break;
+         }else if(favColor[3] == answer){
+            alert('Yes, it is a one of my favourte colore');
+            correct=correct+1;
+            break;
+        }
+        else{
+            answer = prompt(`Try again, you lost ${j} attempts, you have now only ${atempts} attempts`);
+        }
+    }
+
+alert(`You have got ${correct} out of 7 score`);
 alert(`Thank you very much ${name}, for spending some of your valuable time on my site.`);
